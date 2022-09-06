a4368324ef877811c553b05cde21f34571da91e7
Rıza Mert Yağcı <rizamertyagci@gmail.com>
2022-09-06 22:07:04 +0300

    kayar eklendi

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -45,6 +45,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [HttpGet("kayar")]
+    public IActionResult kayar()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
